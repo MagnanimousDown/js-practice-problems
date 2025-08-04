085cb7fe17c35c346e2ed8ebab30a2f8e149cf0b
Omkar Aiya <omkaraiya3@gmail.com>
2025-08-04 17:04:30 +0530

    Changed a comment

--- set02/q2_findMax.js
+++ set02/q2_findMax.js
@@ -4,7 +4,7 @@
 // Example: findMax([4, 2, 9, 1]) → 9
 
 function findMax(arr) {
-    // let maxNum = 0;         // I wonder how will the logic be, if we had [-1, -2, -3, -4] as array and the answer should be -1 here but it will return 0 according to our logic. So, to counter this, use arr[0] as assignment to the maxNum variable
+    // let maxNum = 0;         // I wonder how will the logic be, if we had [-1, -2, -3, -4] as array [ANSWER TO THE DOUBT] :- the answer should be -1 here but it will return 0 according to our logic. So, to counter this, use arr[0] as assignment to the maxNum variable
     let maxNum = arr[0]
 
     arr.forEach(element => {
